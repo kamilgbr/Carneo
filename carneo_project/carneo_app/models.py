b2191f0abcc730e0a17e54be0ff727f49a446c71
Kamil <gabrysiakkamil22@gmail.com>
2022-11-07 13:35:17 +0100

    Updated models. Added img_source field, changed fields types.

--- carneo_project/carneo_app/models.py
+++ carneo_project/carneo_app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+TYPE = (
+    ('diesel', 'Diesel'),
+    ('benzyna', 'Benzyna'),
+    ('benzyna_lpg', 'Benzyna+LPG'),
+    ('hybryda', 'Hybryda'),
+    ('elektryczny', 'Elektryczny')
+)
+
 
 class Cars(models.Model):
     brand = models.CharField(max_length=255)
-    model = models.TextField()
-    engine_type = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    engine_type = models.CharField(choices=TYPE, max_length=255)
     engine_power = models.IntegerField()
     url = models.URLField()
-    ad_source = models.TextField()
+    ad_source = models.URLField()
+    img_source = models.URLField(null=True)
